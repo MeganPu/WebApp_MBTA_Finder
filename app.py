3de2ce7e09e3295f11b27781dec35ec4e970e5e5
MeganPu <62252428+MeganPu@users.noreply.github.com>
2020-04-19 12:28:51 -0400

    final project

--- app.py
+++ app.py
@@ -4,17 +4,28 @@ from mbta_finder import find_stop_near
 
 app = Flask(__name__)
 
+@app.route("/")
+def index():
+    return render_template("index.html")
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/find/', methods=['GET', 'POST'])
 def find():
     # modify this function so it renders different templates for POST and GET method.
     # aka. it displays the form when the method is 'GET'; it displays the results when
     # the method is 'POST' and the data is correctly processed.
     if request.method == 'POST':
-        ...
-    else:
-        ...
+        place = str(request.form["place"])
+        route_type=str(request.form["route_type"])
+        station_name, wheelchair_boarding = find_stop_near(place,route_type)
+
+        if station_name:
+            return render_template(
+                "result.html",place=place,route_type=route_type,station_name=station_name, wheelchair_boarding=wheelchair_boarding
+            )
+        else:
+            return render_template("index.html",error=True)
+    return render_template("index.html",error=None)
 
 
 
--- mbta_finder.py
+++ mbta_finder.py
@@ -1,87 +1,73 @@
-import json
 import urllib.request
+import json
 from pprint import pprint
 
-
-# Useful URLs (you need to add the appropriate parameters for your requests)
 MAPQUEST_BASE_URL = "http://www.mapquestapi.com/geocoding/v1/address"
 MBTA_BASE_URL = "https://api-v3.mbta.com/stops"
 
-# Your API KEYS (you need to use your own keys - very long random characters)
-MAPQUEST_API_KEY = ""
-MBTA_API_KEY = ""
+MAPQUEST_API_KEY = 'FjopzYjCGZVg2Z7qAWrFgkPGnMjw5Lgp'
+MBTA_KEY='07c74aa5c40746f08920ea14b006a76f'
 
-
-# A little bit of scaffolding if you want to use it
 def get_json(url):
-    """
-    Given a properly formatted URL for a JSON web API request, return
-    a Python JSON object containing the response to that request.
-    We did similar thing in the previous assignment.
-    """
-
-    response_data = ...
+ 
+    f = urllib.request.urlopen(url)
+    response_data = json.loads(f.read().decode('utf-8'))
     return response_data
 
 
 def get_lat_long(place_name):
-    """
-    Given a place name or address, return a (latitude, longitude) tuple
-    with the coordinates of the given place.
-    See https://developer.mapquest.com/documentation/geocoding-api/address/get/
-    for Mapquest Geocoding API URL formatting requirements.
-    """
+   
     place = place_name.replace(' ', '%20')
     url = f'{MAPQUEST_BASE_URL}?key={MAPQUEST_API_KEY}&location={place}'
     # print(url) # uncomment to test the url in browser
     place_json = get_json(url)
     # pprint(place_json)
-    lat = place_json[...][...] # modify this so you get the correct latitude
-    lon = place_json[...][...] # modify this so you get the correct longitude
+    lat = place_json["results"][0]["locations"][0]['displayLatLng']['lat'] # modify this so you get the correct latitude
+    lon = place_json["results"][0]["locations"][0]['displayLatLng']['lng'] # modify this so you get the correct longitude
 
-    return lat, lon
+    return lat,lon
 
 
-def get_nearest_station(latitude, longitude):
-    """
-    Given latitude and longitude strings, return a (station_name, wheelchair_accessible)
-    tuple for the nearest MBTA station to the given coordinates.
-    See https://api-v3.mbta.com/docs/swagger/index.html#/Stop/ApiWeb_StopController_index for URL
-    formatting requirements for the 'GET /stops' API.
-    """
-
-    url = f'{MBTA_BASE_URL}?api_key={MBTA_API_KEY}&filter[latitude]={latitude}&filter[longitude]={longitude}&sort=distance'
+def get_nearest_station(latitude, longitude,route_type):
+    
+#	Choose the type of transportation used on a route. Valid options are:
+# 0 - Tram, Streetcar, Light rail. 
+# 1 - Subway, Metro. 
+# 2 - Rail. 
+# 3 - Bus. 
+# 4 - Ferry. 
+# 5 - Cable tram. 
+# 6 - Aerial lift, suspended cable car (e.g., gondola lift, aerial tramway). 
+# 7 - Funicular. 
+# 11 - Trolleybus. 
+# 12 - Monorail. 
+    url = f'{MBTA_BASE_URL}?api_key={MBTA_KEY}&filter[latitude]={latitude}&filter[longitude]={longitude}&sort=distance&route_type={route_type}&radius=0.08'
     # print(url) # uncomment to test the url in browser
     station_json = get_json(url)
     # pprint(station_json) # uncomment to see the json data
-    station_name = station_json[...][...] # modify this so you get the correct station name
-    # print(station_name) # uncomment to check it
+    station_name = station_json["data"][0]['attributes']['name'] 
+    # print(station_name) # uncomment to check it0
 
     # try to find out where the wheelchair_boarding information is
-    wheelchair_boarding = ...
+    wheelchair_boarding = station_json["data"][0]['attributes']['wheelchair_boarding']
 
     return station_name, wheelchair_boarding
 
-
-def find_stop_near(place_name):
-    """
-    Given a place name or address, return the nearest MBTA stop and whether it is wheelchair accessible.
-    You don't need to modify this function
-    """
-    return get_nearest_station(*get_lat_long(place_name))
-
+def find_stop_near(place_name,route_type):
+    return get_nearest_station(*get_lat_long(place_name),route_type)
 
 def main():
     # final test here
     place = input('Enter a place name in Boston such as "Fenway Park": ')
-    lat, lon = get_lat_long(place)
-    print(lat, lon)
-    print(get_nearest_station(lat, lon))
+    route_type=input('RouteType:')
+    lat,lon = get_lat_long(place)
+    print(lat,lon)
+    print(get_nearest_station(lat,lon,route_type))
 
     # final wrap-up
-    print(find_stop_near(place))
+    print(find_stop_near(place,route_type))
 
 
 if __name__ == '__main__':
     main()
-
+    
